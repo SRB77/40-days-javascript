ada19da69756d48914f9f460f9827378233a7693
SRB77 <soumyach144@gmail.com>
2026-08-17 01:43:31 +0530

    almost done with Day-01

---
+++ Day-01/first.js
@@ -0,0 +1,5 @@
+console.log("Hii testing Javascript");
+
+document.getElementById('some-id').innerText = "i am some div";
+
+//> document.write("check the browser console for a message") --> Bad practice
